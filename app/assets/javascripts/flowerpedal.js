c4acb892f9d06d08cec7731bf618d55b47a8e5e3
Louis Vichy <lgvichy@gmail.com>
2015-05-23 15:39:16 -0700

    Louis' commit

---
+++ app/assets/javascripts/flowerpedal.js
@@ -0,0 +1,5 @@
+var tag1 = document.getElementById('tag1');
+var tag2 = document.getElementById('tag2');
+var tag3 = document.getElementById('tag3');
+var tag4 = document.getElementById('tag4');
+var tag5 = document.getElementById('tag5');
